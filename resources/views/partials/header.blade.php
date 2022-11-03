112201d4c43878a4ee692021f1bd2c9e1918b71f
oocengg <fauzinugraha06@gmail.com>
2022-11-03 18:04:02 +0700

    Upload UI, partials & layouts, Home Page

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,25 @@
+<!-- ======= Header ======= -->
+<header id="header" class="header fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <a href="{{ url('home') }}" class="logo d-flex align-items-center me-auto me-lg-0">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1>Katteri<span>.</span></h1>
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="{{ url('home') }}">Home</a></li>
+          <li><a href="#">Menu List</a></li>
+          <li><a href="#">Your Food</a></li>
+          <li><a href="#">Profile User</a></li>
+        </ul>
+      </nav><!-- .navbar -->
+
+      <a class="btn-book-a-table" href="#">Book Now!</a>
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+
+    </div>
+  </header><!-- End Header -->
