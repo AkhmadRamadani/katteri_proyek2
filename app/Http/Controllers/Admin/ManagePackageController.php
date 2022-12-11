cd2ff31e1d4050ce47fc51046b71acd53ff190cf
Akhmad Ramadani <akhmdramadani18@gmail.com>
2022-12-11 18:23:30 +0700

    add crud package

--- app/Http/Controllers/Admin/ManagePackageController.php
+++ app/Http/Controllers/Admin/ManagePackageController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\JadwalMakananModel;
+use App\Models\MenuModel;
+use App\Models\PaketModel;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -15,7 +18,14 @@ class ManagePackageController extends Controller
      */
     public function index()
     {
-        return view('admin.views.package.manage_package');
+        // search package by name
+        if (request()->has('search')) {
+            $packages = PaketModel::where('nama_paket', 'like', '%' . request()->search . '%')->paginate(10);
+            return view('admin.views.package.manage_package', ['packages' => $packages]);
+        }
+        // get package
+        $packages = PaketModel::all();
+        return view('admin.views.package.manage_package', ['packages' => $packages]);
     }
 
     /**
@@ -36,7 +46,15 @@ class ManagePackageController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // store image 
+        $fileToUpload = $request->file('foto')->store('packages', 'public');
+        $package = new PaketModel();
+        $package->nama_paket = $request->nama_paket;
+        $package->deskripsi = $request->deskripsi_paket;
+        $package->harga_paket = $request->harga_paket;
+        $package->foto = $fileToUpload;
+        $package->save();
+        return redirect()->route('paket.index');
     }
 
     /**
@@ -47,7 +65,31 @@ class ManagePackageController extends Controller
      */
     public function show($id)
     {
-        return view('admin.views.package.detail_package');
+        # get package with its relationshin in jadwal_makanan, menu 
+        $package =  PaketModel::with(['jadwal_makanan' => function ($query) {
+            $query->orderBy('hari_id', 'asc');
+        }, 'jadwal_makanan.menu', 'jadwal_makanan.hari'])->find($id);
+        
+        /// group menu by hari and add them to new array and it to package
+        $menus = [
+            'Senin' => [],
+            'Selasa' => [],
+            'Rabu' => [],
+            'Kamis' => [],
+            'Jumat' => [],
+            'Sabtu' => [],
+            'Minggu' => []
+        ];
+        foreach ($package->jadwal_makanan as $jadwal) {
+            $menus[$jadwal->hari->nama_hari][] = $jadwal->menu->id;
+        }
+        $package->menus = $menus;
+
+
+        
+
+        $menus = MenuModel::all();
+        return view('admin.views.package.detail_package', ['package' => $package, 'menus' => $menus]);
     }
 
     /**
@@ -70,7 +112,91 @@ class ManagePackageController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // dd($request->all());
+        $package = PaketModel::find($id);
+        $package->nama_paket = $request->nama_paket;
+        $package->deskripsi = $request->deskripsi_paket;
+        $package->harga_paket = $request->harga_paket;
+        if ($request->hasFile('foto')) {
+            $fileToUpload = $request->file('foto')->store('packages', 'public');
+            $package->foto = $fileToUpload;
+        }
+        $package->save();
+
+        $jadwalMakanan = new JadwalMakananModel();
+        if ($request->has('menu_senin')) {
+            $jadwalMakanan->where('paket_id', $id)->where('hari_id', 1)->delete();
+            foreach ($request->menu_senin as $menu) {
+                $jadwalMakanan->create([
+                    'paket_id' => $id,
+                    'hari_id' => 1,
+                    'menu_id' => $menu
+                ]);
+            }
+        }
+        if ($request->has('menu_selasa')) {
+            $jadwalMakanan->where('paket_id', $id)->where('hari_id', 2)->delete();
+            foreach ($request->menu_selasa as $menu) {
+                $jadwalMakanan->create([
+                    'paket_id' => $id,
+                    'hari_id' => 2,
+                    'menu_id' => $menu
+                ]);
+            }
+        }
+        if ($request->has('menu_rabu')) {
+            $jadwalMakanan->where('paket_id', $id)->where('hari_id', 3)->delete();
+            foreach ($request->menu_rabu as $menu) {
+                $jadwalMakanan->create([
+                    'paket_id' => $id,
+                    'hari_id' => 3,
+                    'menu_id' => $menu
+                ]);
+            }
+        }
+        if ($request->has('menu_kamis')) {
+            $jadwalMakanan->where('paket_id', $id)->where('hari_id', 4)->delete();
+            foreach ($request->menu_kamis as $menu) {
+                $jadwalMakanan->create([
+                    'paket_id' => $id,
+                    'hari_id' => 4,
+                    'menu_id' => $menu
+                ]);
+            }
+        }
+        if ($request->has('menu_jumat')) {
+            $jadwalMakanan->where('paket_id', $id)->where('hari_id', 5)->delete();
+            foreach ($request->menu_jumat as $menu) {
+                $jadwalMakanan->create([
+                    'paket_id' => $id,
+                    'hari_id' => 5,
+                    'menu_id' => $menu
+                ]);
+            }
+        }
+        if ($request->has('menu_sabtu')) {
+            $jadwalMakanan->where('paket_id', $id)->where('hari_id', 6)->delete();
+            foreach ($request->menu_sabtu as $menu) {
+                $jadwalMakanan->create([
+                    'paket_id' => $id,
+                    'hari_id' => 6,
+                    'menu_id' => $menu
+                ]);
+            }
+        }
+        if ($request->has('menu_minggu')) {
+            $jadwalMakanan->where('paket_id', $id)->where('hari_id', 7)->delete();
+            foreach ($request->menu_minggu as $menu) {
+                $jadwalMakanan->create([
+                    'paket_id' => $id,
+                    'hari_id' => 7,
+                    'menu_id' => $menu
+                ]);
+            }
+        }
+
+
+        return redirect()->route('paket.index');
     }
 
     /**
@@ -81,6 +207,9 @@ class ManagePackageController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $package = PaketModel::find($id);
+        $package->delete();
+
+        return redirect()->route('paket.index');
     }
 }
--- resources/views/admin/views/package/manage_package.blade.php
+++ resources/views/admin/views/package/manage_package.blade.php
@@ -1,157 +1,141 @@
 @extends('admin.index')
 @section('content')
-<!-- content @s -->
-<div class="nk-content " style="padding: 0">
-    <div class="container">
-        <div class="nk-content-inner">
-            <div class="nk-content-body">
-                <div class="nk-block-head nk-block-head-sm">
-                    <div class="nk-block-between">
-                        <div class="nk-block-head-content">
-                            <h3 class="nk-block-title page-title">Packages</h3>
-                        </div><!-- .nk-block-head-content -->
-                        <div class="nk-block-head-content">
-                            <div class="toggle-wrap nk-block-tools-toggle">
-                                <a href="#" class="btn btn-icon btn-trigger toggle-expand mr-n1"
-                                    data-target="pageMenu"><em class="icon ni ni-more-v"></em></a>
-                                <div class="toggle-expand-content" data-content="pageMenu">
-                                    <ul class="nk-block-tools g-3">
-                                        <li>
-                                            <div class="form-control-wrap">
-                                                <div class="form-icon form-icon-right">
-                                                    <em class="icon ni ni-search"></em>
-                                                </div>
-                                                <input type="text" class="form-control" id="default-04"
-                                                    placeholder="Quick search by id">
-                                            </div>
-                                        </li>
-                                        <li class="nk-block-tools-opt">
-                                            <a href="#" data-target="addProduct"
-                                                class="toggle btn btn-icon btn-primary d-md-none"><em
-                                                    class="icon ni ni-plus"></em></a>
-                                            <a href="#" data-target="addProduct"
-                                                class="toggle btn btn-primary d-none d-md-inline-flex"><em
-                                                    class="icon ni ni-plus"></em><span>Add Package</span></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div><!-- .nk-block-head-content -->
-                    </div><!-- .nk-block-between -->
-                </div><!-- .nk-block-head -->
-                <div class="nk-block">
-                    <div class="row g-gs">
-                        <div class="col-xxl-3 col-lg-4 col-sm-6">
-                            <div class="card card-bordered product-card">
-                                <div class="product-thumb">
-                                    <a href="/admin/paket/1">
-                                        <img class="card-img-top" src="{{ asset('assets/img/menu/menu-item-2.png') }}"
-                                            alt="" style="padding: 40px">
-                                    </a>
-                                </div>
-                                <div class="card-inner text-center">
-                                    <ul class="product-tags">
-                                        <li><a href="/admin/paket/1">Package</a></li>
-                                    </ul>
-                                    <h5 class="product-title"><a href="/admin/paket/1">Weight Loss
-                                            Package</a></h5>
-                                    <div class="product-price text-primary h5"> Rp. 800.000,-</div>
-                                </div>
-                            </div>
-                        </div><!-- .col -->
-                        <div class="col-xxl-3 col-lg-4 col-sm-6">
-                            <div class="card card-bordered product-card">
-                                <div class="product-thumb">
-                                    <a href="html/product-details.html">
-                                        <img class="card-img-top" src="{{ asset('assets/img/menu/menu-item-2.png') }}"
-                                            alt="" style="padding: 40px">
-                                    </a>
-                                </div>
-                                <div class="card-inner text-center">
-                                    <ul class="product-tags">
-                                        <li><a href="#">Package</a></li>
-                                    </ul>
-                                    <h5 class="product-title"><a href="html/product-details.html">Weight Loss
-                                            Package</a></h5>
-                                    <div class="product-price text-primary h5"> Rp. 800.000,-</div>
-                                </div>
-                            </div>
-                        </div><!-- .col -->
-                        <div class="col-xxl-3 col-lg-4 col-sm-6">
-                            <div class="card card-bordered product-card">
-                                <div class="product-thumb">
-                                    <a href="html/product-details.html">
-                                        <img class="card-img-top" src="{{ asset('assets/img/menu/menu-item-2.png') }}"
-                                            alt="" style="padding: 40px">
-                                    </a>
-                                </div>
-                                <div class="card-inner text-center">
-                                    <ul class="product-tags">
-                                        <li><a href="#">Package</a></li>
-                                    </ul>
-                                    <h5 class="product-title"><a href="html/product-details.html">Weight Loss
-                                            Package</a></h5>
-                                    <div class="product-price text-primary h5"> Rp. 800.000,-</div>
-                                </div>
-                            </div>
-                        </div><!-- .col -->
-                    </div>
-                </div><!-- .nk-block -->
-                <div class="nk-add-product toggle-slide toggle-slide-right" data-content="addProduct"
-                    data-toggle-screen="any" data-toggle-overlay="true" data-toggle-body="true" data-simplebar>
-                    <div class="nk-block-head">
-                        <div class="nk-block-head-content">
-                            <h5 class="nk-block-title">New Package</h5>
-                            <div class="nk-block-des">
-                                <p>Add information and add new package.</p>
-                            </div>
-                        </div>
-                    </div><!-- .nk-block-head -->
-                    <div class="nk-block">
-                        <div class="row g-3">
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <label class="form-label" for="product-title">Package Title</label>
-                                    <div class="form-control-wrap">
-                                        <input type="text" class="form-control" id="product-title">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <label class="form-label" for="description">Description</label>
-                                    <div class="form-control-wrap" style="height: 150px;">
-                                        <textarea type="text" class="form-control" id="description"></textarea>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <label class="form-label" for="product-title">Price</label>
-                                    <div class="form-control-wrap">
-                                        <input type="text" class="form-control" id="product-title">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <label class="form-label">Photo</label>
-                                <div class="upload-zone small bg-lighter my-2">
-                                    <div class="dz-message">
-                                        <span class="dz-message-text">Drag and drop file</span>
-                                    </div>
-                                </div>
-                            </div>
+    <!-- content @s
+        -->
+        <div class="nk-content " style="padding: 0">
+            <div class="container">
+                <div class="nk-content-inner">
+                    <div class="nk-content-body">
+                        <div class="nk-block-head nk-block-head-sm">
+                            <div class="nk-block-between">
+                                <div class="nk-block-head-content">
+                                    <h3 class="nk-block-title page-title">Packages</h3>
+                                </div><!-- .nk-block-head-content -->
+                                <div class="nk-block-head-content">
+                                    <div class="toggle-wrap nk-block-tools-toggle">
+                                        <a href="#" class="btn btn-icon btn-trigger toggle-expand mr-n1"
+                                            data-target="pageMenu"><em class="icon ni ni-more-v"></em></a>
+                                        <div class="toggle-expand-content" data-content="pageMenu">
+                                            <ul class="nk-block-tools g-3">
+                                                <li>
+                                                    <div class="form-control-wrap">
+                                                        <div class="form-icon form-icon-right">
+                                                            <em class="icon ni ni-search"></em>
+                                                        </div>
+                                                        <form class="form-inline" action="{{ route('paket.index') }}">
+                                                            <div class="form-group">
+                                                                <input type="text" class="form-control" placeholder="Search"
+                                                                    name="search">
+                                                            </div>
+                                                            <div class="mx-1">
+                                                                <button type="submit" class="btn btn-primary">Search</button>
 
-                            <div class="col-12">
-                                <button class="btn btn-primary"><em class="icon ni ni-plus"></em><span>Add
-                                        New</span></button>
+                                                            </div>
+                                                        </form>
+
+
+                                                    </div>
+                                                </li>
+                                                <li class="nk-block-tools-opt">
+                                                    <a href="#" data-target="addProduct"
+                                                        class="toggle btn btn-icon btn-primary d-md-none"><em
+                                                            class="icon ni ni-plus"></em></a>
+                                                    <a href="#" data-target="addProduct"
+                                                        class="toggle btn btn-primary d-none d-md-inline-flex"><em
+                                                            class="icon ni ni-plus"></em><span>Add Package</span></a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div><!-- .nk-block-head-content -->
+                            </div><!-- .nk-block-between -->
+                        </div><!-- .nk-block-head -->
+                        <div class="nk-block">
+                            <div class="row g-gs">
+                                @foreach ($packages as $item)
+                                    <div class="col-xxl-3 col-lg-4 col-sm-6">
+                                        <div class="card card-bordered product-card">
+                                            <div class="product-thumb">
+                                                <a href="{{ route('paket.show', $item->id) }}">
+                                                    <img class="card-img-top" src="{{ asset('storage/' . $item->foto) }}"
+                                                        alt="" style="padding: 40px">
+                                                </a>
+                                            </div>
+                                            <div class="card-inner text-center">
+                                                <ul class="product-tags">
+                                                    <li><a href="{{ route('paket.show', $item->id) }}">Package</a></li>
+                                                </ul>
+                                                <h5 class="product-title"><a href="{{ route('paket.show', $item->id) }}">{{ $item->nama_paket }}</a>
+                                                </h5>
+                                                <div class="product-price text-primary h5"> @money($item->harga_paket)</div>
+                                            </div>
+                                        </div>
+                                    </div><!-- .col -->
+                                @endforeach
+
+
                             </div>
+                        </div><!-- .nk-block -->
+                        <div class="nk-add-product toggle-slide toggle-slide-right" data-content="addProduct"
+                            data-toggle-screen="any" data-toggle-overlay="true" data-toggle-body="true" data-simplebar>
+                            <div class="nk-block-head">
+                                <div class="nk-block-head-content">
+                                    <h5 class="nk-block-title">New Package</h5>
+                                    <div class="nk-block-des">
+                                        <p>Add information and add new package.</p>
+                                    </div>
+                                </div>
+                            </div><!-- .nk-block-head -->
+                            <div class="nk-block">
+                                <form action="{{ route('paket.store') }}" method="post" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="row g-3">
+                                        <div class="col-12">
+                                            <div class="form-group">
+                                                <label class="form-label" for="product-title">Package Title</label>
+                                                <div class="form-control-wrap">
+                                                    <input type="text" class="form-control" id="product-title" name="nama_paket">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="form-group">
+                                                <label class="form-label" for="description">Description</label>
+                                                <div class="form-control-wrap" style="height: 150px;">
+                                                    <textarea type="text" class="form-control" id="description" name="deskripsi_paket"></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="form-group">
+                                                <label class="form-label" for="product-title">Price</label>
+                                                <div class="form-control-wrap">
+                                                    <input type="text" class="form-control" id="product-title" name="harga_paket">
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-12">
+                                            <div class="form-group">
+                                                <label class="form-label" for="product-title">Price</label>
+                                                <div class="form-control-wrap">
+                                                    <input type="file" class="form-control" id="product-title" name="foto">
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-12">
+                                            <button type="submit" class="btn btn-primary"><em class="icon ni ni-plus"></em><span>Add
+                                                    New</span></button>
+                                        </div>
+                                    </div>
+                                </form>
+
+                            </div><!-- .nk-block -->
                         </div>
-                    </div><!-- .nk-block -->
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-<!-- content @e -->
-@endsection
+        <!-- content @e -->
+    @endsection
